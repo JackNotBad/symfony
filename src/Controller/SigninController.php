6c285eaf46fc5a3ffd4faeaab832cd93018aa1b9
BenjaminS <77944166+JackNotBad@users.noreply.github.com>
2025-09-25 12:09:48 +0200

    Gestion User Partie SignIn 2
    Chiffrer les mots de passe

--- src/Controller/SigninController.php
+++ src/Controller/SigninController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\SigninType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Doctrine\ORM\EntityManagerInterface;
 final class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'app_signin')]
-    public function index(Request $req, EntityManagerInterface $entityManager): Response
+    public function index(Request $req, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
 
@@ -23,6 +24,17 @@ final class SigninController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+            // Faire que le mot de passe soit caché/hashé
+            // Vérifiez que le user posséde bien use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+            // Vérifiez que le fichier packages\security.yaml posséde bien le  https://symfony.com/doc/current/security.html#registering-the-user-hashing-passwords -  password_hashers: - Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface: 'auto'
+            $plaintextPassword = $user->getPassword();
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+
             $entityManager->persist($user);
             $entityManager->flush();
             // Afficher un message de validation
