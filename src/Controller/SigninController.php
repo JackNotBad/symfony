01a155462773f4ab545e077e355e9ff30b24ac97
BenjaminS <77944166+JackNotBad@users.noreply.github.com>
2025-09-25 11:58:35 +0200

    Gestion User Partie SignIn 1
    Création Controller SignIn push précédant
    Création d'un formulaire
    symfony console make:form
    Modification des fichiers pour que le formulaire apparaisse
    Ajout theme pour formulaire de bootstrap
    Personnalisation du thème et ajout du bouton valider
    Ajout du code pour envoyer les informations à la base de donnée

--- src/Controller/SigninController.php
+++ src/Controller/SigninController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\SigninType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'app_signin')]
-    public function index(): Response
+    public function index(Request $req, EntityManagerInterface $entityManager): Response
     {
+        $user = new User();
+
+        $form = $this->createForm(SigninType::class, $user);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // Afficher un message de validation
+        } else {
+            // Afficher une erreur
+        }
         return $this->render('signin/index.html.twig', [
-            'controller_name' => 'SigninController',
+            'form' => $form->createView(),
         ]);
     }
 }
